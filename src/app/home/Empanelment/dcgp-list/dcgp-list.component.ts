be98da504e4758956da118b5c9a4662a6ae3ea1c
ahpl-pankajsajekar <pankaj.sajekar@alineahealthcare.in>
2024-04-19 19:42:32 +0530

    dc gp list

--- src/app/home/Empanelment/dcgp-list/dcgp-list.component.ts
+++ src/app/home/Empanelment/dcgp-list/dcgp-list.component.ts
@@ -1,37 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms'; // Import FormBuilder and FormGroup
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonService } from 'src/app/_services/common.service';
 
 @Component({
   selector: 'app-dcgp-list',
   templateUrl: './dcgp-list.component.html',
   styleUrls: ['./dcgp-list.component.scss']
 })
-export class DcgpListComponent implements OnInit {
-  form: FormGroup = new FormGroup({}); // Declare form as FormGroup
-  providerNames: string[] = []; // Initialize providerNames as an empty array
-  providerIDs: string[] = []; // Initialize providerIDs as an empty array
-  isLinear = true; // Initialize isLinear property
+export class DcgpListComponent {
+  form: any = FormGroup;
+  formData: FormData = new FormData();
 
-  constructor(private fb: FormBuilder) { }
+  PanImageFile: any;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder, 
+    private commonService: CommonService,
+  ) { }
+
+  ngOnInit() {
     this.initForm();
-    // Initialize providerNames and providerIDs arrays if needed
+    this.loadData()
   }
 
-  initForm(): void {
+  initForm() {
     this.form = this.fb.group({
-      Name: [''], // Initialize form controls with default values if needed
-      ID: ['']
+      dcName: ['', Validators.required],
+      pan_image: [this.PanImageFile],
+      aadhar_number: ['', ],
+      pan_number: ['', ],
+      client: ['', ],
     });
   }
 
-  onSubmit(): void {
-    if (this.form.valid) {
-      console.log('Form submitted:', this.form.value);
-      // Perform form submission actions here
-    } else {
-      // Handle form validation errors
+  onSubmit() {
+    console.log('Form submitted:');
+    this.formData.append('dcName', this.form.value.dcName)
+    // this.formData.append('pan_image', this.form.value.pan_image)
+    if (this.PanImageFile) {
+      this.formData.append('pan_image', this.PanImageFile);
     }
+    this.formData.append('pan_number', this.form.value.pan_number)
+    this.formData.append('aadhar_number', this.form.value.aadhar_number)
+    this.formData.append('client', this.form.value.client)
+    if (this.form.valid) {
+      // Process form submission
+      console.log('Form submitted:', this.form.value);
+      const url = "/selfemp/add/" 
+      this.commonService.postMethod(url, this.formData).subscribe(
+        (res: any) => {
+          console.log("res", res)
+        },
+        (err: any) => {
+          console.warn(err)
+        })
+    }
+  }
+
+  employementData : any;
+  loadData(){
+    const url = "/selfempanelments" 
+      this.commonService.getMethod(url).subscribe(
+        (res: any) => {
+          console.log("res", res)
+          this.employementData = res.data
+        },
+        (err: any) => {
+          console.warn(err)
+        })
   }
 }
