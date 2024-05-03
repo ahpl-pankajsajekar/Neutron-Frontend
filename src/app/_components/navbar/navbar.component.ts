3f67837e6735e5cc479efe1bc348d05e923fb99b
ahpl-pankajsajekar <pankaj.sajekar@alineahealthcare.in>
2024-05-03 09:17:10 +0530

    role viz temp

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -19,12 +19,36 @@ export class NavbarComponent {
   
   user?:User | null;
 
+
+  isNetworkRole : boolean = false;
+  isLegalRole : boolean = false;
+  isITRole : boolean = false;
+  isUserNotLoginTemp : boolean = false;
   constructor(private accountService: AccountService){
     this.accountService.user.subscribe(x => this.user = x)
+    this.getRole()
   }
 
   logout(){
     this.accountService.logout()
+  }
+
+  getRole(){
+    const user = this.accountService.userValue
+    const role = user?.role || 4
+    console.warn(role)
+    if (role == String(1)) {
+      this.isNetworkRole = true;
+    }
+    else if(role == String(2)){
+      this.isLegalRole = true
+    }
+    else if(role == String(4)){
+      this.isUserNotLoginTemp = true
+    }
+    else{
+      this.isITRole = true
+    }
   }
 
 }
